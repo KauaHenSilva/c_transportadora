21b828c50abf71cb3bfee3dcad655d8e9c7d5274
rayss4lves <rayssasantos.com890@gmail.com>
2024-07-13 12:33:29 -0300

    add inserir - finalizar

--- src/Rotas/RotaNaoEfetuada/rota_nao_efetuada.c
+++ src/Rotas/RotaNaoEfetuada/rota_nao_efetuada.c
@@ -5,3 +5,26 @@ void hello_word_rotas_nao_efetuadas()
 {
   printf("Hello, World! Rotas não efetuadas\n");
 }
+
+void inserirRotaNaoEfetuada(Pilha *pilha, RotaNaoEfetuada *rotaNaoEfetuada){
+  RotaNaoEfetuada *novo = malloc(sizeof(RotaNaoEfetuada));
+  if(novo == NULL){
+    printf("Erro ao alocar memoria\n");
+  }else{
+  novo = rotaNaoEfetuada;
+  novo->ant = pilha->topo;
+  pilha->topo = novo; 
+  }
+}
+
+void finalizarRotaNaoEfetuada(Pilha *pilha){
+  RotaNaoEfetuada *rota;
+  while(pilha->topo!= NULL){
+    rota = pilha->topo;
+    pilha->topo = pilha->topo->ant;
+    free(rota);
+  }
+  printf("Rotas nao efetuadas finalizadas\n");
+}
+
+
