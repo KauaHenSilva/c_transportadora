40466e1f5b9d7a22e610268f55ff345f4c15e04f
KauaHenSilva <kaua.sbc@gmail.com>
2024-08-07 18:05:33 -0300

    testando

--- src/app/Entrega/Score/score.c
+++ src/app/Entrega/Score/score.c
@@ -1,7 +1,72 @@
-#include <score.h>
-#include <stdio.h>
+#include "score.h"
+#include <stdlib.h>
 
-void hello_word_score()
+typedef struct scoreRota
 {
-  printf("Hello, World! Score\n");
+  int ponto;
+  int idRota;
+  struct scoreRota *prox;
+} ScoreRota;
+
+ScoreRota *getScore(ScoreRota *score, int idRota)
+{
+  ScoreRota *aux = score;
+  while (aux != NULL)
+  {
+    if (aux->idRota == idRota)
+      return aux;
+    aux = aux->prox;
+  }
+  return NULL;
+}
+
+ScoreRota *createScore(ScoreRota *score, int idRota)
+{
+  ScoreRota *newScore = (ScoreRota *)malloc(sizeof(ScoreRota));
+  newScore->ponto = 0;
+  newScore->idRota = idRota;
+  newScore->prox = NULL;
+
+  if (score == NULL)
+  {
+    score = newScore;
+  }
+  else
+  {
+    ScoreRota *aux = score;
+    while (aux->prox != NULL)
+      aux = aux->prox;
+    aux->prox = newScore;
+  }
+
+  return score;
+}
+
+void exibirScore(HistoricoEntrega *historicoEntrega)
+{
+  ScoreRota *score = NULL;
+
+  HistoricoEntrega *aux = historicoEntrega;
+
+  while (aux != NULL)
+  {
+
+    ScoreRota *scoreAux = getScore(score, aux->idRota);
+    if (scoreAux == NULL)
+    {
+      score = createScore(score, aux->idRota);
+      scoreAux = getScore(score, aux->idRota);
+    }
+
+    if (aux->produto->status == ENTREGAR_A_CLINETE)
+      score->ponto += 5;
+    else if (aux->produto->status == REENTREGAR_A_CLINETE)
+      score->ponto += 3;
+    else if (aux->produto->status == DEVOLVER_A_REMETENTE)
+      score->ponto -= 1;
+
+    aux = aux->prox;
+  }
+
+  aux = historicoEntrega;
 }
